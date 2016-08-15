a57a0d69218b199915617b3a5231aebd064cab19
niloufars <niloufar.salehi@gmail.com>
2016-08-14 19:26:00 -0700

    index

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -196,7 +196,7 @@ class TaskController < ApplicationController
       cond = ""
       all_tasks.where(workerID: u).each do |t|
         cond = t.condition
-        if t.tasktype==4
+        if t.tasktype==5
           lev += t.taskstage
         end
         if ( t.condition == 'gp' ) 
